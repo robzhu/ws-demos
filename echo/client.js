e26f7a011db11e2c6fca62545f358b26dfaae8b4
Robert Zhu <robzhu@gmail.com>
2019-01-07 12:42:00 -0500

    split client and clientWithAction

--- echo/client.js
+++ echo/client.js
@@ -1,15 +1,11 @@
 const WebSocket = require('ws');
 const readline = require('readline');
 
-const ws = new WebSocket('ws://localhost:8080');
-ws.on('open', () => {
-    console.log('connected');
-});
+const url = process.argv[2];
+const ws = new WebSocket(url);
 
-ws.on('message', data => {
-    console.log(`From server: ${data}`);
-});
-
+ws.on('open', () => console.log('connected'));
+ws.on('message', data => console.log(`From server: ${data}`));
 ws.on('close', () => {
     console.log('disconnected');
     process.exit();
@@ -18,8 +14,6 @@ ws.on('close', () => {
 readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-}).on('line', input => {
-    console.log(`Sending: ${input}`);
-    const message = JSON.stringify({action: 'echo', data: input});
-    ws.send(message);
+}).on('line', data => {
+    ws.send(data);
 });
